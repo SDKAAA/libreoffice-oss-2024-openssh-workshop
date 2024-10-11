72430ef7c8c1d84338b9fa29417b62c88988f1cd
Bernard Sadaka (SDKAAA) <sdkaaa@gmail.com>
2024-10-11 14:48:19 +0200

    Commented the remove of images; made containers restart unless stoped
    Signed-off-by: Bernard Sadaka (SDKAAA) <sdkaaa@gmail.com>

--- docker/docker_cleanup.sh
+++ docker/docker_cleanup.sh
@@ -2,6 +2,7 @@
 
 sudo docker kill $(sudo docker ps --all | awk '!/CONTAINER/ {print $1}' | xargs)
 sudo docker rm $(sudo docker ps --all | awk '!/CONTAINER/ {print $1}' | xargs)  
-sudo docker rmi $(sudo docker images --all | awk '!/REPOSITORY/ {print $3}' | xargs)
+# uncomment the below line if you need to also clear the images. it was commented in order to avoid needing to re-download the images from the internet after each cleanup; good idea if you break things frequently and/or working on a less reliable internet connection.
+# sudo docker rmi $(sudo docker images --all | awk '!/REPOSITORY/ {print $3}' | xargs)
 sudo docker network prune -f
 sudo docker system prune -f
